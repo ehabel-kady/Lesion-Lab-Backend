3a2a23ef4212a2f0df0e2b770b547ce4100bfe0a
ehabel-kady <ehabadelkady@outlook.com>
2020-01-04 14:10:06 +0200

    creating post scan view

--- brain/views.py
+++ brain/views.py
@@ -6,5 +6,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.utils.translation import gettext as _
 import math
+from brain.models import *
+from brain.serializers import *
 from django.utils import timezone , dateparse
 # Create your views here.
+
+class CreateScans(generics.CreateAPIView):
+    serializer_class = ScanSerializer
+    permission_class = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        """perform create method that returns a scan instance"""
+        return serializer.save()
+    
+    
